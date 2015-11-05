ab157148c903f62d06d45b39caa39a8f58b85fc1
Marcin Lawniczak <marcin.safmb@wp.pl>
2015-11-05 19:52:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## Synopsis
+
+This is a simple AJAX viewer for ShirtBase. It is just a demonstration and lacks adding/deleting/updating features at the moment.
+
+## Motivation
+
+This project was created at the same time as ShirtBase as a part of a school project about APIs, their uses and creation.
+As ShirtBase is an API server, I decided to write a simple consumer for that API.
+
+## Installation
+
+Put it on a web server. Adjust URLs in apiJson.js to match the URLs of your ShirtBase installation.
+
+## Contributors
+
+All support and questions on GitHub.
+
+Marcin Lawniczak <marcin@lawniczak.me>
+
+## License
+
+MIT license, see: LICENSE.md 
---
+++ js/apiJson.js
@@ -0,0 +1,150 @@
+jQuery.fn.outerHTML = function(s) {
+    return (s)
+        ? this.before(s).remove()
+        : jQuery("<p>").append(this.eq(0).clone()).html();
+}
+
+var colors;
+
+$( document ).ready( getColors() );
+
+$( document ).ready( ajaxShirts() );
+
+$(function(){
+    $('#apigetbtn').on('click', function(e) {
+        e.preventDefault();
+        ajaxShirts()
+    });
+    $('.shirtlink').on('click', function(e) {
+        e.preventDefault();
+        ajaxSingleShirt(e.id);
+    });
+});
+
+function ajaxShirts() {
+    var url = 'http://localhost:8000/api/shirts';
+
+    $.ajax({
+        url: url,
+        data: {
+            format: 'json'
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+        },
+        // This needs fixing
+        //dataType: 'jsonp',
+        success: function(data) {
+            clearShirts();
+            $.each(data, function(i, item) {
+                putShirt(data[i]);
+            });
+        },
+        type: 'GET'
+    });
+}
+
+function ajaxSingleShirt(id) {
+    var url = 'http://localhost:8000/api/shirts/';
+
+    $.ajax({
+        url: url + id,
+        data: {
+            format: 'json'
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+        },
+        // This needs fixing
+        //dataType: 'jsonp',
+        success: function(data) {
+            console.log('single one');
+            clearShirts();
+            putShirt(data[0]);
+        },
+        type: 'GET'
+    });
+}
+
+function clearShirts(){
+    $('#shirts').html('');
+    console.log('Cleared shirts');
+}
+
+function getColors() {
+    var url = 'http://localhost:8000/api/colors/';
+
+    $.ajax({
+        url: url,
+        data: {
+            format: 'json'
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(textStatus, errorThrown);
+        },
+        // This needs fixing
+        //dataType: 'jsonp',
+        //async: false,
+        success: function(data) {
+            colors = data;
+            console.log(colors);
+        },
+        type: 'GET'
+    });
+}
+
+function putShirt(shirt) {
+    $shirtDiv = $(
+        "<div class=\"col s3\">" +
+            "<div class=\"grid-item card hoverable\" id=\""+ shirt.id +"\">" +
+                "<div class=\"card-image waves-effect waves-block waves-light\">" +
+                    "<img src=\"http://localhost:8000/images/" + shirt.photo + "\" class=\"activator\">" +
+                "</div>" +
+                "<div class=\"card-content\">"+
+                    "<span class=\"card-title activator grey-text text-darken-4\">" + shirt.name + "<i class=\"material-icons right\">more_vert</i></span>"+
+                "</div>"+
+                "<div class=\"card-reveal\">"+
+                    "<span class=\"card-title grey-text text-darken-4\">" + shirt.name + "<i class=\"material-icons right\">close</i></span>"+
+                    "<table class=\"centered responsive-table\">"+
+                        "<tr>"+
+                        "<td><p id=\"tablebold\">Właściciel</p></td>"+
+                        //shirt.user_id
+                        "<td>" + 'Marcin' + "</td>"+
+                        "</tr>"+
+                        "<tr>"+
+                        "<td><p id=\"tablebold\">Rozmiar</p></td>"+
+                        "<td>" + shirt.size + "</td>"+
+                        "</tr>"+
+                        "<tr>"+
+                        "<td><p id=\"tablebold\">Długość rękawa</p></td>"+
+                        "<td>" + shirt.sleeve_length + "cm</td>"+
+                        "</tr>"+
+                        "<tr>"+
+                        "<td><p id=\"tablebold\">Kolor</p></td>"+
+                        "<td><div class=\"chip\"><img style=\"background-color:" + colors[shirt.color_id-1].hexCode + "\"/>" + colors[shirt.color_id-1].name + "</div></td>"+
+                        "</tr>"+
+                        "<tr>"+
+                        "<td><p id=\"tablebold\">Wygoda</p></td>"+
+                        "<td id=\"comfortability\">"+ "</td>"+
+                        "</tr>"+
+                        "<tr>"+
+                        "<td><p id=\"tablebold\">Zużycie</p></td>"+
+                        "<td id=\"wear\">"+
+                        "</td>"+
+                        "</tr>"+
+                        "</table>"+
+                        "</div>" +
+                        "</div>"+
+                        "</div>"
+    );
+
+    for($i=0;$i < shirt.comfortability;$i++){
+        $shirtDiv.find('#comfortability').append('<i class=\"material-icons tiny\">grade</i>');
+    }
+
+    for($i=0;$i < shirt.wear;$i++){
+        $shirtDiv.find('#wear').append('<i class=\"material-icons tiny\">grade</i>');
+    }
+
+    $('#shirts').append($shirtDiv);
+}
